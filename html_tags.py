017c92f698603219506d028a01bf9859094cd13c
malik <malikrishit0522@gmail.com>
2023-10-29 11:26:35 +0530

    Deleted 5 files

--- html_tags.py
+++
@@ -1,4 +0,0 @@
-import re
-text = "<html><head><title>Sample Page</title></head><body><h1>Hello, World!</h1></body></html>"
-pattern = r"<.*?>"
-print(re.findall(pattern, text))
--- phone_numbers.py
+++
@@ -1,7 +0,0 @@
-# Match and extract all phone numbers in the format "(123) 456-7890" or "123-456-7890" from a given text.
-import re
-text = "Contact us at (123) 456-7890 or 555-555-5555."
-pattern = r"(\(\d{3}\) \d{3}-\d{4})|(\d{3}-\d{3}-\d{4})"
-matches = re.findall(pattern, text)
-
-print(matches)
